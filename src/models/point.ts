4ccfc7ee7d4803bf21e76f79e52bf5bfaf0f7c98
Leandro Del Arco <leandrodelarco@outlook.com>
2023-05-07 07:53:43 -0300

    Create helper functions in class Point

--- src/models/point.ts
+++ src/models/point.ts
@@ -1,4 +1,23 @@
 export class Point {
 
     constructor(public x: number = 0, public y: number = 0) { }
+
+    public clone(): Point {
+
+        return new Point(this.x, this.y);
+    }
+
+    public static distance(p1: Point, p2: Point): number {
+
+        return Math.sqrt(
+            (p2.x - p1.x) * (p2.x - p1.x)
+            +
+            (p2.y - p1.y) * (p2.y - p1.y)
+        );
+    }
+
+    public distance(p: Point): number {
+
+        return Point.distance(this, p);
+    }
 }
