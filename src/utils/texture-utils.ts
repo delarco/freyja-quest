b7306e0911af0be067c18b9890fe21f1fd45e43a
Leandro Del Arco <leandrodelarco@outlook.com>
2023-05-16 01:19:02 -0300

    Remove texture tests from assets manager

--- src/utils/texture-utils.ts
+++ src/utils/texture-utils.ts
@@ -1,9 +1,15 @@
 import { Color } from "../models/color";
 import { Size } from "../models/size";
 import { Texture } from "../models/texture";
+import { ArrayUtils } from "./array-utils";
 
 export class TextureUtils {
 
+    /**
+     * Serialize a texture into TEX format
+     * @param texture 
+     * @returns 
+     */
     public static serialize(texture: Texture): Uint8Array {
 
         // 30 bytes: name
@@ -32,6 +38,11 @@ export class TextureUtils {
         return data;
     }
 
+    /**
+     * Deserialize a texture from TEX format
+     * @param data 
+     * @returns 
+     */
     public static deserialize(data: Uint8Array): Texture {
 
         // 30 bytes: name
@@ -68,5 +79,220 @@ export class TextureUtils {
         }
 
         return new Texture(name, new Size(width, height), arrayColors);
+    }
+
+    /**
+     * Generates and returns a test texture.
+     */
+    private static makeTestTexture(name: string, size: Size): Texture {
+
+        const texture = new Texture(name, size, new Array<Color>());
+
+        for (let y of ArrayUtils.range(size.height)) {
+
+            for (let x of ArrayUtils.range(size.width)) {
+
+                let color = new Color(255, 255, 255, 0);
+
+                if (x == 0 && y != 0) color = Color.RED;
+
+                if (y == 0 && x != 0) color = Color.GREEN;
+
+                if (x == size.width - 1 && y != 0) color = Color.BLUE;
+
+                if (y == size.height - 1 && x != 0) color = Color.ORANGE;
+
+                if (
+                    (x == 0 && y == 0)
+                    || (x == size.width - 1 && y == 0)
+                    || (x == 0 && y == size.height - 1)
+                    || (x == size.width - 1 && y == size.height - 1)
+                    || (x == y)
+                    || (x == 2 && y == 1)
+                    || (x == 1 && y == 2)
+                ) color = Color.RED;
+
+                texture.data.push(color);
+            }
+        }
+
+        return texture;
+    }
+
+    /**
+     * Generates and returns a night gradient skybox.
+     * @param height 
+     * @param debugBorders 
+     * @returns 
+     */
+    public static makeSkyBoxNightTexture(height: number, debugBorders: boolean = false): Texture {
+
+        const texture = new Texture('skybox-night', new Size(360, height));
+
+        for (let y of ArrayUtils.range(texture.size.height)) {
+            for (let x of ArrayUtils.range(texture.size.width)) {
+
+                let color = (x < 180)
+                    ? new Color(
+                        x * 255 / texture.size.width,
+                        0,
+                        y * 255 / texture.size.height
+                    )
+                    : new Color(
+                        ((360 - x) * 255 / texture.size.width),
+                        0,
+                        y * 255 / texture.size.height
+                    );
+
+
+                if (
+                    debugBorders && (
+                        x == 0
+                        || y == 0
+                        || x == texture.size.width - 1
+                        || y == texture.size.height - 1
+                    )
+                ) color = Color.RED;
+
+                texture.drawPixel(x, y, color);
+            }
+        }
+
+        if (debugBorders) texture.drawPixel(1, 1, Color.ORANGE);
+
+        return texture;
+    }
+
+    /**
+     * Generates and returns a day gradient skybox.
+     * @param height 
+     * @param debugBorders 
+     * @returns 
+     */
+    public static makeSkyBoxDayTexture(height: number, debugBorders: boolean = false): Texture {
+
+        const texture = new Texture('skybox-day', new Size(360, height));
+
+        for (let y of ArrayUtils.range(texture.size.height)) {
+            for (let x of ArrayUtils.range(texture.size.width)) {
+
+                let color = (x < 180)
+                    ? new Color(
+                        255 - (x * 255 / texture.size.width),
+                        255 - (x * 255 / texture.size.width),
+                        (y * 255 / texture.size.height)
+                    )
+                    : new Color(
+                        255 - ((360 - x) * 255 / texture.size.width),
+                        255 - ((360 - x) * 255 / texture.size.width),
+                        (y * 255 / texture.size.height)
+                    )
+
+                if (
+                    debugBorders && (
+                        x == 0
+                        || y == 0
+                        || x == texture.size.width - 1
+                        || y == texture.size.height - 1
+                    )
+                ) color = Color.RED;
+
+                texture.drawPixel(x, y, color);
+            }
+        }
+
+        if (debugBorders) texture.drawPixel(1, 1, Color.ORANGE);
+
+        return texture;
+    }
+
+    /**
+     * Generate a bricks-like test texture
+     * @param size 
+     * @param fillColor 
+     * @param debugBorders 
+     * @returns 
+     */
+    public static makeBricksTexture(size: Size, fillColor: Color, debugBorders: boolean = false): Texture {
+
+        const texture = new Texture('bricks', size);
+
+        for (let y of ArrayUtils.range(size.height)) {
+
+            for (let x of ArrayUtils.range(size.width)) {
+
+                let color = (
+                    y == 0
+                    || (x == size.width / 2 && y < size.height / 2)
+                    || (y == size.height / 2)
+                    || (x == Math.trunc(size.width / 4) && y > size.height / 2)
+                    || (x == Math.trunc(size.width / 4) * 3 && y > size.height / 2)
+                )
+                    ? new Color(99, 69, 44)
+                    : fillColor//new Color(200, 140, 90)
+
+                color = (
+                    debugBorders
+                    && (
+                        x == 0
+                        || y == 0
+                        || x == size.width - 1
+                        || y == size.height - 1
+                    )
+                )
+                    ? Color.BLACK
+                    : color
+
+                texture.data.push(color)
+            }
+        }
+
+        if (debugBorders) {
+
+            texture.drawPixel(1, 1, Color.RED);
+            texture.drawPixel(texture.size.width - 2, texture.size.height - 2, Color.GREEN);
+        }
+
+        return texture;
+    }
+
+    /**
+     * Generate a wall test texture.
+     * @param size 
+     * @param fillColor 
+     * @param debugBorders 
+     * @returns 
+     */
+    public static makeWallTexture(size: Size, fillColor: Color, debugBorders: boolean = false): Texture {
+
+        const texture = new Texture('wall', size);
+
+        for (let y of ArrayUtils.range(size.height)) {
+
+            for (let x of ArrayUtils.range(size.width)) {
+
+                let color = (
+                    debugBorders
+                    && (
+                        x == 0
+                        || y == 0
+                        || x == size.width - 1
+                        || y == size.height - 1
+                    )
+                )
+                    ? Color.BLACK
+                    : fillColor
+
+                texture.data.push(color);
+            }
+        }
+
+        if (debugBorders) {
+
+            texture.drawPixel(1, 1, Color.RED);
+            texture.drawPixel(texture.size.width - 2, texture.size.height - 2, Color.GREEN);
+        }
+
+        return texture;
     }
 }
